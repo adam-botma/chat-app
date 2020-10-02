71d4fe39382641afac255531fd8e90c58fb3f61b
Adam Botma <adam.botma@me.com>
2020-10-02 02:14:47 +0000

    latest update

---
+++ build/precache-manifest.dfb09a7531ce044ec9e9b7e9ace4a8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99132c5c910aa95126adb0b1657fb8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ca5a29faa331b4bdfe",
+    "url": "/static/css/main.064c1e15.chunk.css"
+  },
+  {
+    "revision": "21d21f365a74ee731240",
+    "url": "/static/js/2.0a2bece5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.0a2bece5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ca5a29faa331b4bdfe",
+    "url": "/static/js/main.aa28cf44.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  },
+  {
+    "revision": "fffe2ed2df27ee346b9711a15a14f8d8",
+    "url": "/static/media/class-notes-logo-2.fffe2ed2.png"
+  }
+]);
